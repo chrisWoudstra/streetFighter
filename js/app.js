2e335952e7131957ffae486e68c20634dc145dab
Chris Woudstra <woudstr2@gmail.com>
2015-09-08 00:39:18 -0400

    adding intro theme

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
 
 $(document).ready(function() {
+    doIntro();
+    playGame();
+});
+
+var ryuIntro = false; function ryuTheme () {
+  ryuIntro = !ryuIntro; 
+  if (ryuIntro) {
+    $('#ryu-theme')[0].play();
+  }
+}
+
+var mcHammer = false; 
+function hammerTime () {
+  mcHammer = !mcHammer;
+  if (mcHammer) {
+    $('#ryu-theme')[0].pause();
+    $('#hammer-time')[0].play();
+  }
+}
+
+ function playGame() { 
   $('.ryu').mouseenter(function() {
     $('.ryu-action').hide();
     $('.ryu-ready').show();
@@ -23,20 +44,42 @@ $(document).ready(function() {
   .mouseup(function() {
     $('.ryu-action').hide();
     $('.ryu-ready').show();
-  })
+  });
+
   $(document).keydown(function(e) {
     if (e.which == 88) {
+      hammerTime();
       $('.ryu-action').hide();
       $('.ryu-cool').show();
     }
   })
   .keyup(function(e) {
     if (e.which == 88) {
+      $('#hammer-time')[0].pause();
+      $('#hammer-time')[0].load();
       $('.ryu-cool').hide();
       $('.ryu-ready').show();
     }
   });
-});
+}
+
+function doIntro() {
+  $('#ryu-theme')[0].volume = 0.4;
+  $('#ryu-theme')[0].play();
+  $('.sf-logo').fadeIn(3500, function() {
+    $(this).fadeOut(1000, function() {
+      $('.round').fadeIn(1500, function() {
+        $(this).fadeOut(1000, function() {
+          $('.fight').fadeIn(1500, function() {
+            $(this).fadeOut(1500, function() {
+              $('.instructions').fadeIn(1000);
+            });
+          })
+        })
+      })
+    })
+  })
+}
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
